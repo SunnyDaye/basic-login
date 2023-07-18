44bb24b4e7ba3c096e9c990e8200c62207614c35
Suncerie Daye <sunceried@gmail.com>
2023-07-17 18:29:35 -0700

    basic node web server running

---
+++ src/server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const {PORT} = process.env;
+
+const app = require("./app");
+
+const listener = () => console.log(`Listening on Port ${PORT}!`);
+
+app.listen(PORT,listener);
