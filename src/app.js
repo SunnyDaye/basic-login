c892ba8ac89287e8f8132bf81c0c35317d7c7c3c
Suncerie Daye <sunceried@gmail.com>
2023-07-19 12:06:11 -0700

    register page implemented

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ app.get('/',(req,res) => {
     res.render('index');
 });
 
+app.get('/register', (req,res) => {
+    res.render('register');
+})
+
 //TODO: Implement not found handler
 app.use((req, res, next) => {
     next({ status: 404, message: `Not found: ${req.originalUrl}` });
