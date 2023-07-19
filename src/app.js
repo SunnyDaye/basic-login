5d5b6a373c4a48e9d6659de029206c71e194dd4e
Suncerie Daye <sunceried@gmail.com>
2023-07-19 07:34:43 -0700

    Particles.js implemented

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set('views', path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname)));
 app.use(express.json());
 
+
 app.get('/',(req,res) => {
     res.render('index');
 });
