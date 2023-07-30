0ba0601ca9c7832a11f1a4e4826fd5596b830be2
Suncerie Daye <sunceried@gmail.com>
2023-07-29 22:23:18 -0700

    Registration page posting user data to database

--- src/app.js
+++ src/app.js
@@ -1,74 +1,66 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
-const { Client } = require('pg');
-const {CONNECTION_URL} = process.env;
-const queries = require('./queries/queries');
+const { Client } = require("pg");
+const { CONNECTION_URL } = process.env;
+const queries = require("./queries/queries");
 const app = express();
 
-
 const database = new Client({
-    connectionString:CONNECTION_URL,
+  connectionString: CONNECTION_URL,
 });
 
 database.connect();
 
 
-
-database.query(queries.selectAllUsers(), (err, res) => {
-    console.log(err, res);
-})
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname)));
 app.use(express.json());
 
-
-app.get('/',(req,res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/register', (req,res) => {
-    res.render('register',{error:null});
+app.get("/register", (req, res) => {
+  res.render("register", { error: null });
 });
 
-app.post('/register', (req, res) => {
-    const values = [...Object.values(req.body)];
-    database.query(queries.insertUser(values), (err,data) => {
-        
-        if(err){
-            let error = "Something failed on our end! Please try again.";
-            if(err.code === '23505'){
-                error = "That email is already taken. Please try another.";
-            }
-            console.log(error);
-            return res.render("register", {error: error});
-        }else{
-            return res.redirect("/dashboard"); 
-        }
-    });
-    
+app.post("/register",  (req, res, next) => {
+  const values = [...Object.values(req.body)];
+  
+    database.query(queries.insertUser(values), (err, data) => {
+    if (err) {
+      if (err.code === "23505") {
+        next({ status: 400, message: "Email already taken. Please try another" });
+
+      }else{
+        next({});
+      }
+    }else {
+      res.status(200).json({registrationSuccess: true});
+    }
+  });
 });
 
-app.get('/login', (req,res) => {
-    res.render('login');
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.get('/dashboard', (req,res) => {
-    res.render('dashboard');
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
 });
 
 //TODO: Implement not found handler
 app.use((req, res, next) => {
-    next({ status: 404, message: `Not found: ${req.originalUrl}` });
+  next({ status: 404, message: `Not found: ${req.originalUrl}` });
 });
 
 //TODO: Implement error handler
-app.use((error, req, res, next) => {  // ! Handles all errors
-    console.error(error);
-    const { status = 500, message = "Something went wrong!" } = error;
-    res.status(status).json({ error: message });
-  });
-
-
+app.use((error, req, res, next) => {
+  // ! Handles all errors
+  console.error(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
+});
 
 module.exports = app;
